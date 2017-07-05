6c4a4858770a9690a2018f28285ef64ed3809057
Elias <elias@eliasschulze.se>
2017-07-05 11:25:40 +0200

    Update parser.c

--- parser.c
+++ parser.c
@@ -40,14 +40,11 @@ void initParser() {
     parser->previous = parser->current;
 }
 
-// This function kicks of the whole recursive descent. It's the
-// only function the use code has to call.
 Node* parse() {
     if (!parser) {
         error();
     }
 
-    // Go into recursive descent and return the result.
     return expression();
 }
 
@@ -55,122 +52,34 @@ static Node* expression() {
     return term(); 
 }
 
-/* 
-
-   term -> factor (("-" | "+") factor)*
- 
-   The evolution of the AST visualized. Note that leftChild and
-   rightChild might extend a complete tree themselves, but we
-   connect both of them with a completely new and unconnected
-   operator node
-  
-   1.              (ast)
-  
-   2.           (leftChild)
-  
-   3.       (leftChild) (parent)
-  
-   4.(leftChild) (parent) (rightChild)
-  
-   5.            (parent)
-   /        \
-   (leftChild)          (rightChild)
-  
-   5.            (ast)
-   /     \
-   (leftChild)       (rightChild)
-   
-*/  
 static Node* term() {   
-    Node* ast = factor(); // 1.
+    Node* ast = factor();
     
-    while (match(TOKEN_PLUS) || match(TOKEN_MINUS)) {
-        advance();
-
-        Node* leftChild = ast; // 2.
-
-        // 3.
-        Value* op;
-        switch (parser->previous.type) {
-        case TOKEN_PLUS:
-            op = consOp(OP_ADD);
-            break;
-        case TOKEN_MINUS:
-            op = consOp(OP_SUB);
-            break;
-        default:
-            error();
-        }
-        Node* parent = malloc(sizeof(Node));
-        
-        Node* rightChild = factor(); // 4.
-
-        // 5.
-        parent->value = op;
-        parent->lc = leftChild;
-        parent->rc = rightChild;
-        
-        ast = parent; // 6.
+    while (maa(TOKEN_PLUS) || maa(TOKEN_MINUS)) {
+        Value* val = tokenToValue(parser->previous);
+        Node* n = consNode(ast, val, factor());        
+        ast = n;
     }
 
     return ast;
 }
 
-/* 
-
-   factor -> primary (("*" | "/") primary)*
-   
-*/
 static Node* factor() {
     Node* ast = primary();
 
-    while (match(TOKEN_MUL) || match(TOKEN_DIV)) {
-        advance();
-
-        Value* op;
-        switch (parser->previous.type) {
-        case TOKEN_MUL:
-            op = consOp(OP_MUL);
-            break;
-        case TOKEN_DIV:
-            op = consOp(OP_DIV);
-            break;
-        default:
-            error();
-        }
-        Node* parent = malloc(sizeof(Node));
-        
-        Node* rightChild = primary();
-
-        parent->value = op;
-        parent->lc = ast;
-        parent->rc = rightChild;
-
-        ast = parent;
+    while (maa(TOKEN_MUL) || maa(TOKEN_DIV)) {
+        Value* val = tokenToValue(parser->previous);
+        Node* n = consNode(ast, val, primary());
+        ast = n;
     }
 
     return ast;
 }
 
-/* 
-
-   primary -> NUM
-   
-*/
 static Node* primary() {
-    if (match(TOKEN_NUMBER)) {
-        advance();
-       
-        // Convert lexeme of type string to type long long.
-        char* lexemeStart = parser->previous.start;
-        long long num = strtoll(lexemeStart, NULL, 10);
-       
-        Node* prim = malloc(sizeof(Node));
-       
-        prim->value = consNum(num);       
-        prim->lc = NULL;
-        prim->rc = NULL;
-       
+    if (maa(TOKEN_NUMBER)) {
+        Value* val = tokenToValue(parser->previous);
+        Node* n = consNode(NULL, val, NULL);
         return prim;
     }
 
