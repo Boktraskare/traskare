70dd8770f5c86e2329a91346404efea4d23155ff
Elias <elias@eliasschulze.se>
2017-07-05 11:36:13 +0200

    Update ast.h

--- ast.h
+++ ast.h
@@ -9,9 +9,6 @@
 #ifndef ast_h
 #define ast_h
 
-
-// Indicates what type of operator is contained in an OPERATOR
-// ValueType.
 typedef enum {
     OP_ADD,
     OP_SUB,
@@ -19,15 +16,11 @@ typedef enum {
     OP_DIV
 } Operator;
 
-// Union tag.
 typedef enum {
     OPERATOR,
     NUMBER,
 } ValueType;
 
-// A tagged union. The union can hold any of its members. The
-// tag indicates which one it is so we know how to read its
-// value.
 typedef struct {
     ValueType type;
     union {
@@ -37,15 +30,14 @@ typedef struct {
 } Value;
 
 typedef struct Node Node;
-
 typedef struct Node {
     Value* value;
     Node*  lc;
     Node*  rc;
 } Node;
 
-Value* consOp(Operator);
-Value* consNum(long long);
+Node* consNode(Node*, Value*, Node*)
+Value* tokenToValue(Token);
 void printAst(Node*);
 
 #endif
