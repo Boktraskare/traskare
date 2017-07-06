ea291ee40329968fd400c91dafe288bb72c436bb
Elias <elias@eliasschulze.se>
2017-07-06 12:56:34 +0200

    Update eval.c

--- eval.c
+++ eval.c
@@ -1,43 +1,32 @@
-/*
- *  
- *
- *  This file contains what is needed to evaluate expressions.
- *
- *  Need a function for parentheses and for binary expressions 
- *
- *
- *
- */
+/* ----------------------------------------------------------
 
-#include "eval.h"
+   EVALUATION OF AST
+   
+   This file contains what is needed to evaluate expressions.
+   
+   ---------------------------------------------------------- */
+
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-long long binary(Node* ast);
+#include "eval.h"
 
+long long binary(Node*);
 
-//Evaluating binary expressions of numbers and simple math
-long long binary(Node* ast){
+// Evaluating binary expressions of numbers and simple math
+long long binary(Node* ast) {
     long long right = ast->rc->value->content.value;
     long long left = ast->lc->value->content.value;
 
-    switch(ast->value->content.op){
-        case OP_ADD:
-            return (long long) left + (long long) right;
-
-        case OP_SUB:
-            return (long long) left - (long long) right;
-
-        case OP_MUL:
-            return (long long) left * (long long) right;
-
-        case OP_DIV:
-            return (long long) left / (long long) right;
-    
+    switch(ast->value->content.op) 
+        case OP_ADD: return left + right;
+        case OP_SUB: return left - right;
+        case OP_MUL: return left * right;
+        case OP_DIV: return left / right;    
     }
 
-    return false; //Very temporary
+    return false; // Very temporary
 }
 
 
